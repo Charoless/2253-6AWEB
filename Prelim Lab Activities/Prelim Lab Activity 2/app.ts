ae6b852e6b2daf392638cdbe6e4a7139bb9b05fd
Charles Joseph M. Antonio <chrlsjsphantnio@gmail.com>
2025-11-17 17:43:20 +0800

    Add files via upload

---
+++ Prelim Lab Activities/Prelim Lab Activity 2/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { Header } from './header/header';
+import { Skills } from './skills/skills';
+import { Footer } from './footer/footer';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Header, Skills, Footer],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class App {
+  protected readonly title = signal('my-first-app');
+}
